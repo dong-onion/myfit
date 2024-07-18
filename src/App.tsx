0cfd440e092dc9b5f5d796b81a7ea42ea07839f0
dong-onion <>
2024-07-19 04:31:05 +0900

    style: 컴포넌트 경로 정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ServiceRegistration from './pages/ServiceRegistration';
-import Header from './componenets/Header';
+import { Header } from './componenets';
 
 const App = () => {
   return (
--- src/componenets/Header/Header.tsx
+++ src/componenets/Header/Header.tsx
@@ -1,35 +1,13 @@
 import React from 'react';
 import { backArrow, logo } from '@/assets';
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 60px;
-  width: 100dvw;
-  border-bottom: 1px solid #e5e5e5;
-`;
-
-export const Logo = styled.img`
-  width: 73px;
-  height: 24px;
-`;
-
-export const BackArrow = styled.img`
-  position: absolute;
-  width: 24px;
-  height: 24px;
-  left: 51px;
-  top: 19px;
-`;
+import * as S from './Header.style';
 
 const Header = () => {
   return (
-    <Container>
-      <Logo src={logo} alt="logo" />
-      <BackArrow src={backArrow} alt="backArrow" />
-    </Container>
+    <S.Container>
+      <S.Logo src={logo} alt="logo" />
+      <S.BackArrow src={backArrow} alt="backArrow" />
+    </S.Container>
   );
 };
 
