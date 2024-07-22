cecced54a1edb8ca47be2a9c3c80b46146355388
dong-onion <>
2024-07-22 18:57:01 +0900

    1차 결과화면 마크업 완성

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,23 @@
 import logo from './logo.png';
 import backArrow from './back_arrow.png';
 import starGray from './star_gray.png';
+import swotStrength from './swot_strength.png';
+import swotWeakness from './swot_weakness.png';
+import swotOpportunity from './swot_opportunity.png';
+import swotThreat from './swot_threat.png';
+import strategy1 from './strategy1.png';
+import strategy2 from './strategy2.png';
+import strategy3 from './strategy3.png';
 
-export { logo, backArrow, starGray };
+export {
+  logo,
+  backArrow,
+  starGray,
+  swotStrength,
+  swotWeakness,
+  swotOpportunity,
+  swotThreat,
+  strategy1,
+  strategy2,
+  strategy3,
+};
