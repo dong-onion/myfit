067d8245aecb88d8412f74c865d192fa3cd63915
dong-onion <>
2024-07-25 06:39:11 +0900

    환경변수 적용

--- src/api/swot.ts
+++ src/api/swot.ts
@@ -2,8 +2,7 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 import axios from 'axios';
 
-const API_URL =
-  'https://clovastudio.stream.ntruss.com/testapp/v1/chat-completions/HCX-003';
+const API_URL = process.env.REACT_APP_API_URL;
 const HEADERS = {
   'X-NCP-CLOVASTUDIO-API-KEY': process.env.REACT_APP_CLOVA_STUDIO_API_KEY,
   'X-NCP-APIGW-API-KEY': process.env.REACT_APP_APIGW_API_KEY,
